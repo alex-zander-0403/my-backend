f2b7dcc2a2faed3fa73881afa23230058c31f7c3
alex-zander-0403 <alexzander0403@gmail.com>
2026-02-18 01:20:33 +0300

    add endpoint with params

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const myUsers = [
+  { id: 1, name: "Alex", hasCar: true },
+  { id: 2, name: "Bob", hasCar: false },
+  { id: 3, name: "Carl", hasCar: true },
+];
+
 //
 app.get("/", (req, res) => {
-  res.send("Hello, main page1223!");
+  res.send("Hello, main page!!!");
 });
 
-app.post("/", (req, res) => {
-  res.send("New World!");
+app.get("/users", (req, res) => {
+  res.json(myUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  const foundedUser = myUsers.find((user) => user.id === Number(req.params.id));
+
+  if (!foundedUser) {
+    res.status(404).send("Страница не найдена!");
+    return;
+  }
+
+  res.json(foundedUser);
 });
 
 //
