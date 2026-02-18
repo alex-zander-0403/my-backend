e0b56f5fa30c26de423cfad95a9740273770451e
alex-zander-0403 <alexzander0403@gmail.com>
2026-02-18 07:12:25 +0300

    add delete endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const myUsers = [
+app.use(express.json());
+
+let myUsers = [
   { id: 1, name: "Alex", age: 33, hasCar: true },
   { id: 2, name: "Bob", age: 45, hasCar: false },
   { id: 3, name: "Carl", age: 23, hasCar: true },
 ];
 
-//
+// ===== GET =====
 app.get("/", (req, res) => {
   res.send("Hello, main page!!!");
 });
@@ -34,6 +36,41 @@ app.get("/users/:id", (req, res) => {
   }
 
   res.json(foundedUser);
+});
+
+// ===== POST =====
+// fetch('http://localhost:3000/users', {
+//     method: "POST",
+//     headers: {'content-type': 'application/json'},
+//     body: JSON.stringify({ name: "David", age: 100, hasCar: true}) })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+
+app.post("/users", (req, res) => {
+  // const { name, age, hasCar } = req.body;
+
+  const newUser = {
+    id: Number(new Date()),
+    name: req.body.name,
+    age: req.body.age,
+    hasCar: req.body.hasCar,
+  };
+
+  myUsers.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// ===== DELETE =====
+// fetch('http://localhost:3000/users/1', {
+//     method: "DELETE"})
+
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+
+app.delete("/users/:id", (req, res) => {
+  myUsers = myUsers.filter((user) => user.id !== Number(req.params.id));
+
+  res.status(204);
 });
 
 //
